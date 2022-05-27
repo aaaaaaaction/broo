8624bf956789d76561a7e834c5c93c617e22a610
aaaaaaaction <103316303+aaaaaaaction@users.noreply.github.com>
2022-05-27 22:45:23 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,8 +9,8 @@ if [[ $TUNNEL_TOKEN ]]; then
 echo 'has tunnel token, run cloudflared tunnel'
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /root/cloudflared
 chmod +x /root/cloudflared
-/root/brook  wssserver --domainaddress $APP_DOMAIN:443 --password $UUID & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN & nginx -c /root/nginx.conf -g 'daemon off;'
+/root/brook  wsserver -l :443 -p $UUID & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN & nginx -c /root/nginx.conf -g 'daemon off;'
 else
-/root/brook  wssserver --domainaddress $APP_DOMAIN:443 --password $UUID & nginx -c /root/nginx.conf -g 'daemon off;'
+/root/brook  wsserver -l :443 -p $UUID & nginx -c /root/nginx.conf -g 'daemon off;'
 fi
 
